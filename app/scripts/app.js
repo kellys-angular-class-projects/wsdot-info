a723df86c17841d1e077e60a777fa3cb2977a6dc
nyapple <taratarjoto@gmail.com>
2014-08-20 21:21:00 -0700

    Checking in Angular Yeoman files

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name wsdotInfoApp
+ * @description
+ * # wsdotInfoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('wsdotInfoApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wsdotInfoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the wsdotInfoApp
+ */
+angular.module('wsdotInfoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
